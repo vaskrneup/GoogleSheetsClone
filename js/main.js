27c7688d6bbcd8d3009b542e91eca729aa19067a
vaskrneup <bhaskar.neupane.58@gmail.com>
2021-01-24 23:59:54 +0545

    Navbar to rename document complete !!

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 import {Excel} from "./component/excel.js";
 
-function main() {
+// Selectors !!
+const documentNameDOM = document.getElementById('current-doc-name-input');
+
+// END Selectors !!
+
+const changeInputSizeOnInput = (e) => {
+    e.target.style.width = ((e.target.value.length + 1) * 9) + 'px';
+}
+
+const functionHandleEvents = () => {
+    documentNameDOM.addEventListener('input', changeInputSizeOnInput)
+}
+
+const main = () => {
     const excel = new Excel(100, 26, 'excel-table');
     excel.render()
 }
 
 main();
+functionHandleEvents();
