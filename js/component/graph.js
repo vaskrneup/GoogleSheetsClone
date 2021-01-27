423fb2a0fd359d75648bddd9e45fa3bcb148212e
vaskrneup <bhaskar.neupane.58@gmail.com>
2021-01-27 17:19:04 +0545

    Initial code for graph completed !!

--- js/component/graph.js
+++ js/component/graph.js
@@ -16,6 +16,7 @@ class Graph {
         this.canvas.width = this.width;
         this.canvas.height = this.height;
         this.ctx = this.canvas.getContext('2d');
+        this.ctx.transform(1, 0, 0, -1, 0, this.height);
     }
 
     createGrid(size) {
@@ -35,20 +36,38 @@ class Graph {
 export class DotGraph extends Graph {
     constructor(xValues, yValues, xAxisLabel, yAxisLabel, dotSize = 2, width = 500, height = 400) {
         super(xValues, yValues, xAxisLabel, yAxisLabel, width, height);
-        this.dotSize = dotSize;
 
+        this.dotSize = dotSize;
         this.modal = new Modal();
+    }
+
+    drawDots = () => {
+        const xAxisGap = (this.width - 10) / Math.max(...this.xValues);
+        const yAxisGap = (this.height - 10) / Math.max(...this.yValues);
+
+        for (let i = 0; i < this.xValues.length; i++) {
+            this.ctx.beginPath();
+            this.ctx.arc(this.xValues[i] * (xAxisGap), this.yValues[i] * yAxisGap, this.dotSize, 0, 2 * Math.PI);
+            this.ctx.stroke();
+            this.ctx.fill();
+            this.ctx.closePath();
+        }
     }
 
     render = () => {
         this._render();
 
-        for (let i = 0; i < this.xValues.length; i++) {
-            this.ctx.fillRect(this.xValues[i] * 10, this.yValues[i] * 10, 3, 3);
-            this.ctx.stroke();
-        }
+        this.drawDots();
 
         this.modal.addModelBody(this.canvas);
+        this.modal.addStyles({
+            backgroundColor: 'white',
+            border: '1px solid black',
+            boxShadow: '3px 3px 10px black',
+            width: this.width + 'px',
+            height: this.height + 'px',
+        });
+        this.modal.compileStyles();
         this.modal.show();
     }
 }
